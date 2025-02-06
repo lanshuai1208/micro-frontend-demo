93f4824eb6e93f3b770a17086e1cdd20ee44a005
lan-shuai <2812450388@qq.com>
2025-02-06 18:59:05 +0800

    wip: vue2

---
+++ packages/vue2/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app-vue">
+  <button @click="sayName">sayName</button>
+    <!-- <router-view /> -->
+  </div>
+</template>
+<script>
+export default {
+  name: 'App',
+  data(){
+    return {
+      name: 'zhangsan'
+    }
+  },
+  methods: {
+    sayName(){
+      console.log(this.name)
+    }
+  }
+
+}
+</script>
+<style>
+#container {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+#nav {
+  padding: 30px;
+  text-align: center;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+  text-align: center;
+}
+</style>
---
+++ packages/vue2/src/main.ts
@@ -0,0 +1,42 @@
+import Vue from 'vue'
+import App from './App.vue'
+// import router from './router';
+// import store from './store';
+
+Vue.config.productionTip = false
+
+let instance = null;
+console.log(instance);
+
+console.log('App is ', App)
+
+new Vue({
+  // router,
+  render: h => h(App)
+}).$mount('#app-vue')
+
+// export async function bootstrap() {
+//   console.log('vue app bootstraped');
+// }
+// if (!window["__MICRO_WEB__"]) {
+//   instance = new Vue({
+//     router,
+//     // store,
+//     render: h => h(App)
+//   }).$mount('#app-vue')
+// }
+
+// export async function mount() {
+//   instance = new Vue({
+//     router,
+//     // store,
+//     render: h => h(App)
+//   }).$mount('#app-vue')
+// }
+// export async function unmount(ctx: any) {
+//   instance = null;
+//   const { container } = ctx
+//   if (container) {
+//     document.querySelector(container).innerHTML = ''
+//   }
+// }
